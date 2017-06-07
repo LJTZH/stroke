6cbe8c31ec48243ae682f8d1decc29712c667236
LJTZH <1987796302@qq.com>
2017-06-07 16:23:41 +0800

    init

---
+++ stroke/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace stroke
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(651, 395);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 0;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(69, 87);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(509, 397);
+            this.axMapControl1.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(651, 33);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(107, 28);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(509, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(648, 119);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(109, 54);
+            this.btnConfirm.TabIndex = 4;
+            this.btnConfirm.Text = "确定";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 569);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.axMapControl1);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnConfirm;
+    }
+}
+
---
+++ stroke/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;
+
+namespace stroke
+{
+    public partial class Form1 : Form
+    {
+        string roadDataFullName = string.Empty;
+        string shapeFileFullName = string.Empty;
+        IGeometry pGPoint;
+        List<IGeometry> pGeometryList = new List<IGeometry>();
+        List<IGeometry> IWT = new List<IGeometry>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOFD = new OpenFileDialog();
+            pOFD.Multiselect = false;
+            pOFD.Title = "打开路网文件";
+            pOFD.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            pOFD.Filter = "路网文件(*.shp)|*.shp";
+            if (pOFD.ShowDialog() == DialogResult.OK)
+            {
+                roadDataFullName = pOFD.FileName;
+                this.textBox1.Text = roadDataFullName;
+            }
+
+
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            pGeometryList = this.GetGeometry(roadDataFullName);
+            this.strokeIt(pGeometryList);
+        }
+        private List<IGeometry> GetGeometry(string roadDataFullName)
+        {
+            List<IGeometry> pGList = new List<IGeometry>();
+            int index = roadDataFullName.LastIndexOf('\\');
+            string folder = roadDataFullName.Substring(0, index);
+            shapeFileFullName = roadDataFullName.Substring(index + 1);
+            IWorkspaceFactory pWSF = new ShapefileWorkspaceFactoryClass();
+            IFeatureWorkspace pFWS = (IFeatureWorkspace)pWSF.OpenFromFile(folder, 0);
+            IFeatureClass pFC = pFWS.OpenFeatureClass(shapeFileFullName);
+            IFeatureCursor pFeatureCursor = pFC.Search(null, false);
+            IFeature pFeature = pFeatureCursor.NextFeature();
+            while (pFeature != null)
+            {
+                IGeometry pGeometry = pFeature.Shape as IGeometry;//通过shape得到Geometry
+                pGList.Add(pGeometry);
+                pFeature = pFeatureCursor.NextFeature();
+                int i = pGList.Count;
+            }
+            return pGList;
+        }
+        private bool CheckCrosses(IGeometry pGeometry1, IGeometry pGeometry2)
+        {
+
+            IRelationalOperator pRO = pGeometry1 as IRelationalOperator;
+            ITopologicalOperator pTO = pGeometry1 as ITopologicalOperator;
+            if (pRO.Touches(pGeometry2))
+            {
+                pGPoint = pTO.Intersect(pGeometry2, esriGeometryDimension.esriGeometry0Dimension);
+                return true;
+            }
+            else
+                return false;
+        }
+        private double GetAngle(IGeometry pGeometry1,IGeometry pGeometry2)
+        {
+            double pAngle1;
+            double pAngle2;
+            double pAngle=0;
+
+            IPolyline pPolyline1=pGeometry1 as IPolyline;
+            IPoint pPF1 = pPolyline1.FromPoint;
+            IGeometry pGeoPoint1 = pPF1 as IGeometry;
+            IRelationalOperator pRO1 = pGeoPoint1 as IRelationalOperator;
+            if (pRO1.Contains(pGPoint))
+            {
+                pAngle1=this.AngleFromPoint(pGeometry1);
+            }
+            else
+            {
+                pAngle1=this.AngleToPoint(pGeometry1);
+            }
+
+            IPolyline pPolyline2 = pGeometry2 as IPolyline;
+            IPoint pPF2 = pPolyline2.FromPoint;
+            IGeometry pGeoPoint2 = pPF2 as IGeometry;
+            IRelationalOperator pRO2 = pGeoPoint2 as IRelationalOperator;
+            if (pRO2.Contains(pGPoint))
+            {
+                pAngle2 = this.AngleFromPoint(pGeometry2);
+            }
+            else
+            {
+                pAngle2 = this.AngleToPoint(pGeometry2);
+            }
+            if ((pAngle1 * pAngle2) >= 0)
+                pAngle = Math.Abs(pAngle2 - pAngle1);
+            if ((pAngle1 * pAngle2) < 0)
+            {
+                pAngle = Math.Abs(pAngle1) + Math.Abs(pAngle2);
+                if (pAngle > Math.PI)
+                    pAngle = 2 * Math.PI - pAngle;
+            }
+            return pAngle;
+        }
+        private double AngleFromPoint(IGeometry pGeometry)
+        {
+            ISegmentCollection pSC = pGeometry as ISegmentCollection;//可能用到的东西：FromPoint、ToPoint、EnumCurve、EnumSegments---IEnumSegment
+            IEnumSegment pEnumSegment = pSC.EnumSegments;
+            pEnumSegment.Reset();
+            ISegment pSegment;
+            int partIndex = 0;
+            int segmentIndex = 0;
+            pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+            ILine pLine = pSegment as ILine;
+            double pAngle = pLine.Angle;
+            return pAngle;
+
+        }
+        private double AngleToPoint(IGeometry pGeometry)
+        {
+            double pAngle=0;
+            ISegmentCollection pSC = pGeometry as ISegmentCollection;//可能用到的东西：FromPoint、ToPoint、EnumCurve、EnumSegments---IEnumSegment
+            IEnumSegment pEnumSegment = pSC.EnumSegments;
+            pEnumSegment.Reset();
+            ISegment pSegment;
+            int partIndex = 0;
+            int segmentIndex = 0;
+            pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+            int i = 0;
+            while (!pEnumSegment.IsLastInPart())
+            {
+                pEnumSegment.SetAt(0, i);
+                pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+                i++;
+            }
+            pEnumSegment.SetAt(0, i - 1);
+            pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+            ILine pLine = pSegment as ILine;
+            double a = pLine.Angle;
+            if (a > 0 && a <= Math.PI)
+            { pAngle = a - Math.PI; }
+            if (a > -Math.PI && a <= 0)
+            { pAngle = a + Math.PI; }
+            return pAngle;
+        }
+        private void strokeIt(List<IGeometry> pGeometryList) {
+            IGeometry r = pGeometryList[0];
+            IGeometry s = pGeometryList[0];
+            double angle = 2*Math.PI/3;
+            for (int i = 0; i < pGeometryList.Count; i++) {
+                for (int j = 0; j <= pGeometryList.Count; j++) {
+                    if (j == i) {
+                        j++;
+                    }
+                    if (j == pGeometryList.Count) {
+                        IWT.Add(pGeometryList[i]);
+                        pGeometryList.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                    if (CheckCrosses(pGeometryList[i], pGeometryList[j]))
+                    {
+                        double a = GetAngle(pGeometryList[i], pGeometryList[j]);
+                        if (a > angle)
+                        {
+                            angle = a;
+                            r = pGeometryList[i];
+                            s = pGeometryList[j];
+                        }
+                    }                  
+                }
+            }
+            if (pGeometryList.Count == 0)
+            {
+                return;
+            }
+            ITopologicalOperator wtf = r as ITopologicalOperator;
+            IGeometry road = wtf.Union(s);
+            pGeometryList.RemoveAt(pGeometryList.IndexOf(r));
+            pGeometryList.RemoveAt(pGeometryList.IndexOf(s));
+            pGeometryList.Add(road);            
+            strokeIt(pGeometryList);
+        }
+    }
+}
+        #region GetAngle 方法一
+        /*
+        private double GetAngle(IGeometry pGeometry1, IGeometry pGeometry2)
+        {
+            //ILine pLine1 = pGeometry1 as ILine;//此处对象可以变为IPolyline
+            // IPolyline pPolyline = pGeometry1 as IPolyline;//可能用到的东西：FromPoint、ToPoint
+            double pAngle = 0;
+            double pAngle1 = 0;
+            double pAngle2 = 0;
+            ISegmentCollection pSC = pGeometry1 as ISegmentCollection;//可能用到的东西：FromPoint、ToPoint、EnumCurve、EnumSegments---IEnumSegment
+            IEnumSegment pEnumSegment = pSC.EnumSegments;
+            pEnumSegment.Reset();
+            ISegment pSegment;
+            int partIndex = 0;
+            int segmentIndex = 0;
+            pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+            /*
+            object o = Type.Missing;
+            ISegmentCollection pPath = new PathClass();
+            pPath.AddSegment(pSegment, ref o, ref o);
+            IGeometryCollection pPolyline = new PolylineClass();
+            pPolyline.AddGeometry(pPath as IGeometry, ref o, ref o);
+            *//*
+            IGeometry pGS = pSegment as IGeometry;
+            IRelationalOperator pRO = pGS as IRelationalOperator;
+            if (pRO.Contains(pGPoint))
+            {
+                ILine pLine1 = pSegment as ILine;
+                pAngle1 = pLine1.Angle;
+            }
+            else
+            {
+                int i = 0;
+                while (!pEnumSegment.IsLastInPart())
+                {
+                    pEnumSegment.SetAt(0, i);
+                    pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+                    i++;
+                }
+                pEnumSegment.SetAt(0, i - 1);
+                pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+                ILine pLine1 = pSegment as ILine;
+                double a = pLine1.Angle;
+                if (a > 0 && a <= Math.PI)
+                { pAngle1 = a - Math.PI; }
+                if (a > -Math.PI && a <= 0)
+                { pAngle1 = a + Math.PI; }
+            }
+
+            pSC = pGeometry2 as ISegmentCollection;
+            pEnumSegment = pSC.EnumSegments;
+            pEnumSegment.Reset();
+            pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+            pGS = pSegment as IGeometry;
+            pRO = pGS as IRelationalOperator;
+            if (pRO.Contains(pGPoint))
+            {
+                ILine pLine2 = pSegment as ILine;
+                pAngle2 = pLine2.Angle;
+            }
+            else
+            {
+                int i = 0;
+                while (!pEnumSegment.IsLastInPart())
+                {
+                    pEnumSegment.SetAt(0, i);
+                    pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+                    i++;
+                }
+                pEnumSegment.SetAt(0, i - 1);
+                pEnumSegment.Next(out pSegment, ref partIndex, ref segmentIndex);
+                ILine pLine2 = pSegment as ILine;
+                double a = pLine2.Angle;
+                if (a > 0 && a <= Math.PI)
+                { pAngle2 = a - Math.PI; }
+                if (a > -Math.PI && a <= 0)
+                { pAngle2 = a + Math.PI; }
+            }
+
+            if ((pAngle1 * pAngle2) >= 0)
+                pAngle = Math.Abs(pAngle2 - pAngle1);
+            if ((pAngle1 * pAngle2) < 0)
+            {
+                pAngle = Math.Abs(pAngle1) + Math.Abs(pAngle2);
+                if (pAngle > Math.PI)
+                    pAngle = 2 * Math.PI - pAngle;
+            }
+           // return pAngle;
+            /*将一个要素可视化，需要建立一个内存工作空间
+            IFeatureClass pFeatureClass=new FeatureClass() as IFeatureClass ;
+            IFeature pFeature = pFeatureClass.CreateFeature();
+            pFeature = pSegment as IFeature;
+            IFeatureLayer pFeatureLayer = new FeatureLayerClass();
+            pFeatureLayer.FeatureClass = pFeatureClass;
+            axMapControl1.AddLayer(pFeatureLayer);
+            axMapControl1.Refresh();
+            */
+        //}
+        #endregion
+   
